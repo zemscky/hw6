35f978ae5ea7a5aeb7d83811deb0de46865eae50
zemscky <aleksei.zemscky@yandex.ru>
2022-09-11 19:24:31 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,26 @@ public class Main {
         else if (age >=18 && age < 24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет");
         }
-        else {
+        else if (age >=24){
             System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        // Задача 3
+        System.out.println("Задача 3");
+        int capacity = 102;
+        int seatsCount =60;
+        int otherCount = capacity - seatsCount; // стоячие места
+
+        int seatsUsed = 60;
+        int otherUsed = 41;
+        if (seatsUsed < seatsCount) {
+            System.out.println("Еще есть " + (seatsCount - seatsUsed) + " сидячих мест");
+        } else {
+            System.out.println("Сидячих мест нет");
+        }
+        if (otherUsed < otherCount) {
+            System.out.println("Еще есть " + (otherCount - otherUsed) + " стоячих мест");
+        } else {
+            System.out.println("Стоячих мест нет");
         }
 
     }
